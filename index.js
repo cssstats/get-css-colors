4f2829349e95cb8467791f8ff7ae01d4fca36ed3
John Otander <johnotander@gmail.com>
2015-02-02 10:42:29 -0700

    Add initial functionality and tests

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-'use strict';
+'use strict'
 
-module.exports = function getCssColors(options) {
-  options = options || {};
+var cssColorList = require('css-color-list')
+var rgbaRegex = require('rgba-regex')
+var rgbRegex = require('rgb-regex')
+var hslaRegex = require('hsla-regex')
+var hslRegex = require('hsl-regex')
 
-  return true;
+module.exports = function getCssColors(string) {
+  if (typeof string != 'string') {
+    throw new TypeError('get-css-colors expected a string')
+  }
+
+  var colorListRegex = cssColorList().join('|')
+  var rgbOrRgbaRegex = rgbRegex().source + '|' + rgbaRegex().source
+  var hslOrHslaRegex = hslRegex().source + '|' + hslaRegex().source
+  var hexRegex = '#([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?\\b'
+
+  var cssColorRegex = new RegExp(colorListRegex + '|' + rgbOrRgbaRegex + '|' + hslOrHslaRegex + '|' + hexRegex, 'ig')
+
+  return string.match(cssColorRegex)
 }
--- test/test.js
+++ test/test.js
@@ -1,9 +1,14 @@
-var assert = require('assert');
-var getCssColors = require('..');
+var assert = require('assert')
+var getCssColors = require('..')
+
+var stringWithColors = 'foobar hsl(,,) hsla(1, 1.111%, 1.1111%, .8) rgba(123, 123, 123, .8) #fff turtles rebeccapurple'
 
 describe('get-css-colors', function() {
 
   it('should do something awesome', function() {
-    assert.equal(getCssColors(), true);
-  });
-});
+    assert.deepEqual(
+      getCssColors(stringWithColors),
+      ['hsla(1, 1.111%, 1.1111%, .8)', 'rgba(123, 123, 123, .8)', '#fff', 'rebeccapurple']
+    )
+  })
+})
