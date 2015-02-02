10b4cb7ec8fc11223bb5aa7a897a6503f6d0cdd2
John Otander <johnotander@gmail.com>
2015-02-02 10:45:47 -0700

    Add case sensitivity test

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
 var assert = require('assert')
 var getCssColors = require('..')
 
-var stringWithColors = 'foobar hsl(,,) hsla(1, 1.111%, 1.1111%, .8) rgba(123, 123, 123, .8) #fff turtles rebeccapurple'
+var stringWithColors = 'foobar hsl(,,) HSLA(1, 1.111%, 1.1111%, .8) rgba(123, 123, 123, .8) #fff turtles rebeccapurple #123AAA'
 
 describe('get-css-colors', function() {
 
   it('should do something awesome', function() {
     assert.deepEqual(
       getCssColors(stringWithColors),
-      ['hsla(1, 1.111%, 1.1111%, .8)', 'rgba(123, 123, 123, .8)', '#fff', 'rebeccapurple']
+      ['HSLA(1, 1.111%, 1.1111%, .8)', 'rgba(123, 123, 123, .8)', '#fff', 'rebeccapurple', '#123AAA']
     )
   })
 })
